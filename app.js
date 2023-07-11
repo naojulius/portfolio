ff2ddc99e3d44a7522d3af9e03e10c1cc7a60e14
naojulius <naoj8lius@gmail.com>
2023-07-11 09:43:44 +0300

    add: node js server side environment

---
+++ app.js
@@ -0,0 +1,28 @@
+// Imports
+const express = require('express')
+const app = express()
+var cors = require('cors')
+const port = 3000
+
+app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
+    next();
+  });
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'app/css'))
+app.use('/js', express.static(__dirname + 'app/js'))
+app.use('/img', express.static(__dirname + 'app/img'))
+
+app.use(express.static(__dirname + '/app'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('', (req, res) => {
+
+    res.sendFile(path.join(__dirname, 'app/index.html'));
+})
+
+app.listen(port, () => console.info(`Listening on port ${port}`))
